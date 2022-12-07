8959a4e235c9a2f7bc01e8c079ec05b78dc5f479
Christopher Lim <cli3338198@gmail.com>
2022-12-07 10:14:27 -0800

    made test upload form

--- Friender-Backend/app.py
+++ Friender-Backend/app.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 
+# from werkzeug import secure_filename
+
 from flask import (
-    Flask, request, redirect, session, g, jsonify
+    Flask, request, redirect, session, g, jsonify, render_template
 )
 # from flask_debugtoolbar import DebugToolbarExtension
 
@@ -15,18 +17,36 @@ from models import (
     User
 )
 
+
+
 load_dotenv()
 
 app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+
+
 connect_db(app)
-db.create_all()
 
 ##############################################################################
+# TEST ROUTE FOR UPLOADING
+@app.get('/upload')
+def upload_file_form():
+   return render_template('upload.html')
+
+@app.post('/uploader')
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+        #   saves file to root directory
+        # overwrites if have same name
+      f.save(f.filename) 
+      return 'file uploaded successfully'
+
 
 ##############################################################################
 
